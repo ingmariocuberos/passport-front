3c5209047ad7c77ea20c2e3cea7f62dbb980a38c
Mario Cuberos <mario.cuberos@grupokonecta.com>
2023-04-17 23:19:15 -0500

    ✨Feat: Lazy load en el app-routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'tramites',
+    loadChildren: () => import('./tramites/tramites.module').then(m=> m.TramitesModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'tramites'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
